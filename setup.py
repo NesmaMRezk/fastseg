b64aac3db1ffd18dcd5b151e2ccfdbb4c0f93b6a
NesmaMRezk <nesma.rezk@gmail.com>
2024-05-28 11:08:59 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     packages=find_namespace_packages(include=["fastseg", "fastseg.*"]),
     include_package_data=True,
     install_requires=[
-        "geffnet >= 0.9.8",
+        "NesmaMRezk.geffnet >= 0.9.8",
         "Pillow >= 7.0.0",
         "numpy >= 1.18.0",
         "torch >= 1.5.0",
