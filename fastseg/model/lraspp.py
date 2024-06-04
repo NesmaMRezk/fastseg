07bf793ab684c9eaf3c0897cf8adcbb730d46238
NesmaMRezk <nesma.rezk@gmail.com>
2024-06-04 23:28:21 +0300

    Update lraspp.py

--- fastseg/model/lraspp.py
+++ fastseg/model/lraspp.py
@@ -13,8 +13,9 @@ from .base import BaseSegmentation
 
 import torch.nn as nn
 
-class LRASPP(nn.Module):
-     def __init__(self, num_classes, trunk, use_aspp=False, num_filters=128):
+class LRASPP(BaseSegmentation):
+    """Lite R-ASPP style segmentation network."""
+    def __init__(self, num_classes, trunk, use_aspp=False, num_filters=128):
         """Initialize a new segmentation model.
 
         Keyword arguments:
@@ -28,41 +29,32 @@ class LRASPP(nn.Module):
 
         self.trunk, s2_ch, s4_ch, high_level_ch = get_trunk(trunk_name=trunk)
         self.use_aspp = use_aspp
-         
-        # ASPP modules
+
+        # Reduced atrous spatial pyramid pooling
         if self.use_aspp:
             self.aspp_conv1 = nn.Sequential(
-                nn.Conv2d(in_channels, aspp_channels, kernel_size=1, bias=False),
-                nn.BatchNorm2d(aspp_channels),
-                nn.ReLU(inplace=True)
+                nn.Conv2d(high_level_ch, num_filters, 1, bias=False),
+                nn.BatchNorm2d(num_filters),
+                nn.ReLU(inplace=True),
             )
             self.aspp_conv2 = nn.Sequential(
-                nn.Conv2d(in_channels, aspp_channels, kernel_size=1, bias=False),
-                nn.Conv2d(aspp_channels, aspp_channels, kernel_size=3, stride=1, padding=12, dilation=12, bias=False),
-                nn.BatchNorm2d(aspp_channels),
-                nn.ReLU(inplace=True)
+                nn.Conv2d(high_level_ch, num_filters, 1, bias=False),
+                nn.Conv2d(num_filters, num_filters, 3, dilation=12, padding=12),
+                nn.BatchNorm2d(num_filters),
+                nn.ReLU(inplace=True),
             )
             self.aspp_conv3 = nn.Sequential(
-                nn.Conv2d(in_channels, aspp_channels, kernel_size=1, bias=False),
-                nn.Conv2d(aspp_channels, aspp_channels, kernel_size=3, stride=1, padding=24, dilation=24, bias=False),
-                nn.BatchNorm2d(aspp_channels),
-                nn.ReLU(inplace=True)
+                nn.Conv2d(high_level_ch, num_filters, 1, bias=False),
+                nn.Conv2d(num_filters, num_filters, 3, dilation=36, padding=36),
+                nn.BatchNorm2d(num_filters),
+                nn.ReLU(inplace=True),
             )
             self.aspp_pool = nn.Sequential(
                 nn.AdaptiveAvgPool2d(1),
-                nn.Conv2d(in_channels, aspp_channels, kernel_size=1, bias=False),
-                nn.BatchNorm2d(aspp_channels),
-                nn.ReLU(inplace=True)
+                nn.Conv2d(high_level_ch, num_filters, 1, bias=False),
+                nn.BatchNorm2d(num_filters),
+                nn.ReLU(inplace=True),
             )
-    
-            # Convolution to reduce the number of channels
-            self.convs2 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-            self.convs4 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-    
-            # Upsampling layers using strided convolutions
-            self.upsample1 = nn.ConvTranspose2d(aspp_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
-            self.upsample2 = nn.ConvTranspose2d(aspp_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
-            self.upsample3 = nn.ConvTranspose2d(aspp_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
             aspp_out_ch = num_filters * 4
         else:
             self.aspp_conv1 = nn.Sequential(
@@ -76,26 +68,36 @@ class LRASPP(nn.Module):
                 nn.Sigmoid(),
             )
             aspp_out_ch = num_filters
+
+        self.convs2 = nn.Conv2d(s2_ch, 32, kernel_size=1, bias=False)
+        self.convs4 = nn.Conv2d(s4_ch, 64, kernel_size=1, bias=False)
+        self.conv_up1 = nn.Conv2d(aspp_out_ch, num_filters, kernel_size=1)
+        self.conv_up2 = ConvBnRelu(num_filters + 64, num_filters, kernel_size=1)
+        self.conv_up3 = ConvBnRelu(num_filters + 32, num_filters, kernel_size=1)
+        self.last = nn.Conv2d(num_filters, num_classes, kernel_size=1)
+
     def forward(self, x):
-        # ASPP module
-        aspp_features = [
-            self.aspp_conv1(x),
-            self.aspp_conv2(x),
-            self.aspp_conv3(x),
-            self.aspp_pool(x)
-        ]
-        aspp = torch.cat(aspp_features, 1)
+        s2, s4, final = self.trunk(x)
+        if self.use_aspp:
+            aspp = torch.cat([
+                self.aspp_conv1(final),
+                self.aspp_conv2(final),
+                self.aspp_conv3(final),
+                self.aspp_pool(final).repeat(1, 1, final.shape[2], final.shape[3])
+            ], 1)
+        else:
+            aspp = self.aspp_conv1(final) * self.aspp_conv2(final).repeat(1, 1, final.shape[2], final.shape[3])
+        y = self.conv_up1(aspp)
+        y = F.conv_transpose2d(y, self.convs4.weight, stride=2)  # Upsampling with ConvTranspose2d
 
-        # Reduce the number of channels
-        s2 = self.convs2(x)
-        s4 = self.convs4(x)
+        y = torch.cat([y, self.convs4(s4)], 1)
+        y = self.conv_up2(y)
+        y = F.conv_transpose2d(y, self.convs2.weight, stride=2)  # Upsampling with ConvTranspose2d
 
-        # Upsampling
-        up1 = self.upsample1(aspp)
-        up2 = self.upsample2(aspp)
-        up3 = self.upsample3(aspp)
-
-        return s2, s4, up1, up2, up3
+        y = torch.cat([y, self.convs2(s2)], 1)
+        y = self.conv_up3(y)
+        y = self.last(y)
+        return y
 
 class MobileV3Large(LRASPP):
     """MobileNetV3-Large segmentation network."""
