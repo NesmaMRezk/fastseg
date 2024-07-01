52a1170d7474b336d9dadd4a6007a77f21d032a8
NesmaMRezk <nesma.rezk@gmail.com>
2024-07-01 14:55:36 +0300

    Update lraspp.py

--- fastseg/model/lraspp.py
+++ fastseg/model/lraspp.py
@@ -250,7 +250,7 @@ class LRASPP(BaseSegmentation):
         self.convs2 = nn.Conv2d(s2_ch, 32, kernel_size=1, bias=False)
         self.convs4 = nn.Conv2d(s4_ch, 64, kernel_size=1, bias=False)
 
-        print("helllo")
+        print("helllo  2")
         # Apply Tucker decomposition to the segmentation head
         self.conv_up1 = tucker_decompose_conv_layer(nn.Conv2d(aspp_out_ch, num_filters, kernel_size=1), rank=(64, 32, 3, 3))
         self.conv_up2 = tucker_decompose_conv_layer(ConvBnRelu(num_filters + 64, num_filters, kernel_size=1).conv, rank=(num_filters, num_filters + 64))
