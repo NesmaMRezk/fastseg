45c54b9d6755b6cafd15b80818acd6d48d8918c2
NesmaMRezk <nesma.rezk@gmail.com>
2024-07-04 15:50:44 +0300

    Update lraspp.py

--- fastseg/model/lraspp.py
+++ fastseg/model/lraspp.py
@@ -15,7 +15,7 @@ def tucker_decompose_conv_layer(layer, rank):
 
     core_all, factors = partial_tucker(weight, rank=rank, modes=[0, 1])
     #core=core_all[0]
-    core, *factors = core_all
+    core, [*factors] = core_all
     print(factors)
     pointwise_s_to_r = nn.Conv2d(in_channels=core.shape[1], out_channels=core.shape[0],
                                  kernel_size=1, stride=1, padding=0, bias=False)
