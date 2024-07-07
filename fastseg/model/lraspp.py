572f68866399928bb99326e689f1ce87968e4e71
NesmaMRezk <nesma.rezk@gmail.com>
2024-07-07 15:52:43 +0300

    s

--- fastseg/model/lraspp.py
+++ fastseg/model/lraspp.py
@@ -48,7 +48,7 @@ def tucker_decompose_conv_layer(layer, rank):
     return decomposed_layer
 
 
-class LRASPP(BaseSegmentation):
+class LRASPP_tensor(BaseSegmentation):
     """Lite R-ASPP style segmentation network."""
     def __init__(self, num_classes, trunk, use_aspp=False, num_filters=128):
         """Initialize a new segmentation model.
@@ -297,7 +297,7 @@ class ConvBnRelu(nn.Module):
         return self.relu(self.bn(self.conv(x)))
 
 
-class LRASPP_base(BaseSegmentation):
+class LRASPP(BaseSegmentation):
     """Lite R-ASPP style segmentation network."""
     def __init__(self, num_classes, trunk, use_aspp=False, num_filters=128):
         """Initialize a new segmentation model.
@@ -364,11 +364,16 @@ class LRASPP_base(BaseSegmentation):
         s2, s4, final = self.trunk(x)
         
         if self.use_aspp:
+            print("final")
+            print(final)
+            x=self.aspp_pool(final)
+            print("x")
+            print(x)
             aspp = torch.cat([
                 self.aspp_conv1(final),
                 self.aspp_conv2(final),
                 self.aspp_conv3(final),
-                F.interpolate(self.aspp_pool(final), size=final.shape[2:],mode='bilinear', align_corners=True),
+                F.interpolate(x, size=final.shape[2:],mode='bilinear', align_corners=True),
             ], 1)
         else:
             aspp = self.aspp_conv1(final) * F.interpolate(
